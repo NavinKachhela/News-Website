6fced6f3da8c727bd9042f3cf78e037beea3af55
Navin H Kachhela <kachhelanavin@gmail.com>
2020-06-08 23:27:52 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+ $('#mobile-nav-open-btn').click(function () {
+     $('#mobile-nav').css('height', '100%');
+ });
+ $('#mobile-nav-close-btn').click(function () {
+     $('#mobile-nav').css('height', '0%');
+ });
